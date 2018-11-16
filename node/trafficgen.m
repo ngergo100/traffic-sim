e501d803b8f1e22b71790077bf8e388630d0f7fb
Gergely Nemeth <ngergo100@gmail.com>
2018-11-16 09:32:26 +0100

    Adds generator for traffic simulation.

--- node/trafficgen.m
+++ node/trafficgen.m
@@ -4,11 +4,13 @@ close all
 
 models = {
     0, 100/3.6, ChillModel;
-    -20, 100/3.6, IDModel(struct('a_max',1.5, 'b_max',1.67, 'v_0',130/3.6, 'T',1.8, 'h_0',2, 'delta',4, 'L', 4.5));
-    -40, 100/3.6, IDModel(struct('a_max',1.5, 'b_max',1.67, 'v_0',130/3.6, 'T',1.8, 'h_0',2, 'delta',4, 'L', 4.5));
-    -60, 100/3.6, IDModel(struct('a_max',1.5, 'b_max',1.67, 'v_0',130/3.6, 'T',1.8, 'h_0',2, 'delta',4, 'L', 4.5));
-    -80, 100/3.6, IDModel(struct('a_max',1.5, 'b_max',1.67, 'v_0',130/3.6, 'T',1.8, 'h_0',2, 'delta',4, 'L', 4.5));
 };
+
+for i=2:100
+   models{i,1} = -((i-1)*50 + randi([-20 20],1,1));
+   models{i,2} = 100/3.6;
+   models{i,3} = IDModel(struct('a_max',1.5, 'b_max',1.67, 'v_0',130/3.6, 'T',1.8, 'h_0',2, 'delta',4, 'L', 4.5));
+end
 
 for i=1:size(models)
    y0(2*i-1) = models{i,1};
@@ -28,14 +30,14 @@ for i=1:plotcount
    hold on;
    subplot(2,1,1)
    plot(t,y(:,2*i)*3.6)
-   legendInfoVelocity{i} = ['v_' num2str(i)];
+   legendInfoVelocity{i} = ['v_{' num2str(i) '}'];
 end
 
 for i=2:plotcount
     hold on;
     subplot(2,1,2)
     plot(t,y(:,2*i-3)-y(:,2*i-1))
-    legendInfoHeadaway{i-1} = ['h_' num2str(i-1) '_-_' num2str(i)];
+    legendInfoHeadaway{i-1} = ['h_{' num2str(i-1) '}' '_-_{' num2str(i) '}'];
 end
 hold on;
 subplot(2,1,1)
