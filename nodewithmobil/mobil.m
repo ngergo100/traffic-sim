aa03b1c4bb1928b5b9f4a33f258c2ad8bee4f7a8
Gergely Nemeth <ngergo100@gmail.com>
2019-02-21 22:03:51 +0100

    Refactoring MOBIL. Closes #20

--- nodewithmobil/mobil.m
+++ nodewithmobil/mobil.m
@@ -1,13 +1,15 @@
 function ret = mobil(params, t)
-if params.a_n_left(2) > -params.b_max_left && params.a_c_left(2) > params.a_c(2) && params.current_position - params.length >= params.current_position_left_car
+
+desirable_change_to_left = params.a_c_left(2) > params.a_c(2);
+desirable_change_to_right = params.a_c_right(2) > params.a_c(2);
+
+if params.safe_change_to_left && desirable_change_to_left && params.can_change_to_left
     disp(['Lane change to left at time: ' num2str(t)])
-    %% TODO a_c_left
-    ret.a_c = params.a_c;
+    ret.a_c = params.a_c_left;
     ret.chosen_lane = params.left_lane;
-elseif params.a_n_right(2) > -params.b_max_right && params.a_c_right(2) > params.a_c(2) && params.current_position - params.length >= params.current_position_right_car
+elseif params.safe_change_to_right && desirable_change_to_right && params.can_change_to_right
     disp(['Lane change to right at time: ' num2str(t)])
-    %% TODO a_c_right
-    ret.a_c = params.a_c;
+    ret.a_c = params.a_c_right;
     ret.chosen_lane = params.right_lane;
 else
     ret.a_c = params.a_c;
