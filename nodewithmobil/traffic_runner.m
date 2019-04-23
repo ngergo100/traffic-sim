4493ba4254b648f77ebe3fa5086e0fa571bb360b
Gergely Nemeth <ngergo100@gmail.com>
2019-04-23 20:32:58 +0200

    Fine tuning

--- nodewithmobil/traffic_runner.m
+++ nodewithmobil/traffic_runner.m
@@ -5,19 +5,24 @@ clc
 close all
 clear
 
-for index=1:4
-    addpath('./Configurations')
-    should_use_images = false;
-    load_data_set_1
+addpath('./Configurations')
 
+
+
+for index=1:10
+    should_use_images = false;
+    
+    load_data_set_1
+    
     driver = IDModel(struct('a_max',1.2, 'b_max',3, 'v_0',50/3.6, 'T',1.8, 'h_0',1, 'delta',4, 'L',4.2, 'time_to_change_lane',1, 'lane_change_duration',2.0, 'not_paying_attention',[], 'acceleration_threshold',10, 'acceleration_difference_threshold',0.3));
 
     dataset_helper(driver,index)
 
     traffic
-    
-    post_processing
+
+    %post_processing
+ 
+    %animation
     
     clear
 end
- 
--- nodewithmobil/traffic_use_case.m
+++ nodewithmobil/traffic_use_case.m
@@ -6,9 +6,9 @@ addpath('./Configurations')
 should_use_images = false;
 load_data_set_1
 
-driver = IDModel(struct('a_max',1.2, 'b_max',3, 'v_0',50/3.6, 'T',1.8, 'h_0',1, 'delta',4, 'L',4.2, 'time_to_change_lane',1, 'lane_change_duration',2.0, 'not_paying_attention',[], 'acceleration_threshold',10, 'acceleration_difference_threshold',0.3));
+%driver = IDModel(struct('a_max',1.2, 'b_max',3, 'v_0',50/3.6, 'T',1.8, 'h_0',1, 'delta',4, 'L',4.2, 'time_to_change_lane',1, 'lane_change_duration',2.0, 'not_paying_attention',[], 'acceleration_threshold',10, 'acceleration_difference_threshold',0.3));
 
-dataset_helper(driver,2)
+%dataset_helper(driver,7)
 
 traffic
     
