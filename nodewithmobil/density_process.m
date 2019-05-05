55d1d2c085d4f0f654b89c364cfd0888428d966a
Gergely Nemeth <ngergo100@gmail.com>
2019-05-05 14:49:16 +0200

    Adds averaged vehicle density plotting

--- nodewithmobil/density_process.m
+++ nodewithmobil/density_process.m
@@ -1,4 +1,4 @@
-function [count_over_time] = density_process(y, target_line)
+function [count_over_time, jumps, values] = density_process(y, target_line)
 
 positions = y(1:2:end,:);
 count_over_time = zeros(1,size(positions,2));
@@ -7,6 +7,12 @@ for i=1:size(positions,2)
     positions_ith = positions(:,i);
     count_over_time(i) = sum(positions_ith > target_line);
 end
-
+count_changes = diff(count_over_time);
+jumps = find(count_changes>0.5);
+change_value = count_changes(jumps);
+values = [change_value(1)];
+for i=2:length(change_value)
+    values(i) = values(i-1) + change_value(i-1);
+end
 end
 
--- nodewithmobil/vehicle_density_post_processing.m
+++ nodewithmobil/vehicle_density_post_processing.m
@@ -1,6 +1,10 @@
-vehicle_density = density_process(y,target_line);
+[vehicle_density, jumps, values] = density_process(y,target_line);
     
 figure(5)
 hold all;
-plot(t,vehicle_density, 'DisplayName',num2str(index)) 
+%% Real lane change plot
+%plot(t,vehicle_density, 'DisplayName',num2str(index)) 
+
+%% Averaged plot
+plot(jumps*dt,values, 'DisplayName',num2str(index))
 legend('-DynamicLegend');
