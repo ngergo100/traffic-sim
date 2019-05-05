4c44697beef8ff452fb62250eea7c57ddaba6696
Gergely Nemeth <ngergo100@gmail.com>
2019-05-05 16:36:35 +0200

    Adds traffic variation report generator. Fixes auto positioning initial value.

--- nodewithmobil/find_previous_in_lane.m
+++ nodewithmobil/find_previous_in_lane.m
@@ -1,15 +1,17 @@
-function back_position = find_previous_in_lane(index)
+function [back_position, found_previous] = find_previous_in_lane(index)
 
 global models
 
 i = index - 1;
 back_position = 0;
+found_previous = false;
 indexth_car_lane = models{index,2};
 
-while (i > 0 && back_position == 0)
+while (i > 0 && ~found_previous)
    ith_car_lane = models{i,2}; 
    if ith_car_lane == indexth_car_lane
        back_position = models{i,4} - models{i,6}.L;
+       found_previous = true;
    end
    i = i - 1;
 end
