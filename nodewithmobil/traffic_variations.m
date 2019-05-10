0a73c04c6626a0cddd284d2f802c226bc2c030cf
Gergely Nemeth <ngergo100@gmail.com>
2019-05-10 11:52:09 +0200

    More writing

--- nodewithmobil/traffic_variations.m
+++ nodewithmobil/traffic_variations.m
@@ -22,7 +22,7 @@ for index=1:size(variations,1)
     actual_variation = variations(index,:);
     
     for i=1:length(actual_variation)
-        driver = IDModel(struct('a_max',1.2, 'b_max',3, 'v_0',50/3.6, 'T',1.8, 'h_0',1, 'delta',4, 'L',4.2, 'time_to_change_lane',1, 'lane_change_duration',2.0, 'not_paying_attention',[], 'acceleration_threshold',10, 'acceleration_difference_threshold',0.3));
+        driver = IDModel(struct('a_max',1.2, 'b_max',3, 'v_0',50/3.6, 'T',1.8, 'h_0',1, 'delta',4, 'L',4.2, 'time_to_change_lane',1, 'lane_change_duration',2.0, 'not_paying_attention',[], 'acceleration_threshold',10, 'acceleration_difference_threshold',0.3, 'p',0));
 
         dataset_helper(driver, actual_variation(i))
     end
