8d2447d696eb0efa53b41dc2dbbed7534ad0511d
Gergely Nemeth <ngergo100@gmail.com>
2019-05-17 00:29:26 +0200

    More work on final model.

--- eemobil/traffic_runner.m
+++ eemobil/traffic_runner.m
@@ -7,20 +7,20 @@ clear
 
 addpath('./Configurations')
 
-config_count = 18;
+config_count = 1;
 
 for index=1:config_count
     should_use_images = false;
     
     load_data_set_1
     
-    driver = IDModel(struct('a_max',1.2, 'b_max',3, 'v_0',50/3.6, 'T',1.8, 'h_0',1, 'delta',4, 'L',4.2, 'time_to_change_lane',1, 'lane_change_duration',2.0, 'not_paying_attention',[], 'acceleration_threshold',10, 'acceleration_difference_threshold',0.3,'p',0));
+    %driver = IDModel(struct('a_max',1.2, 'b_max',3, 'v_0',50/3.6, 'T',1.8, 'h_0',1, 'delta',4, 'L',4.2, 'time_to_change_lane',1, 'lane_change_duration',2.0, 'not_paying_attention',[], 'acceleration_threshold',10, 'acceleration_difference_threshold',0.3,'p',0));
 
-    dataset_helper(driver,index)
+    %dataset_helper(driver,index)
 
     traffic
 
-    %post_processing
+    post_processing
  
     %animation
     
