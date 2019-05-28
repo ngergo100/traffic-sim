09112f3e1cb6c678e990a3e76e67f730eef78c86
Gergely Nemeth <ngergo100@gmail.com>
2019-05-28 21:46:40 +0200

    Generate new case 5 figures

---
+++ eemobil/case_5.m
@@ -0,0 +1,20 @@
+%% Case 5
+clc
+close all
+clear
+
+global case_name
+
+addpath('./Configurations')
+
+should_use_images = false;
+    
+load_data_for_case_5
+
+traffic
+
+case_name = 'case5';
+
+post_processing_for_documentation
+ 
+animation
